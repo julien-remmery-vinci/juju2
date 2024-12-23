805c774ed37ecdac1630b73f1ec9718493cb99ab
Julien Remmery <julien.remmery@student.vinci.be>
2024-12-23 21:13:58 +0100

    Refactor lexer to use BufReader and enhance token structure with position tracking

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::Read};
+use std::{fs::File, io::{self, Read}};
 
 use super::{error::LexerError, token::Token};
 
@@ -6,55 +6,56 @@ use super::{error::LexerError, token::Token};
  * Takes in an input file.
  * Returns an array of tokens present in the file.
  */
-pub fn lex(mut input_file: File) -> Result<Vec<Token>, LexerError> {
+pub fn lex(reader: &mut io::BufReader<File>) -> Result<Vec<Token>, LexerError> {
     let mut tokens: Vec<Token> = Vec::new();
-    let buf= &mut String::default();
-    input_file.read_to_string(buf)
-        .map_err(LexerError::FileReadError)?;
-    let buf = buf.to_string();
+    let mut buf = String::new();
+    reader.read_to_string(&mut buf).map_err(LexerError::FileReadError)?;
+    
     let mut token: String = String::from("");
     let mut char_seq: String = String::from("");
 
+    // Offset to track the position of a char or line in the file
+    let mut offset = 0;
+    let mut line_counter = 0;
+
     for char in buf.chars() {
-        if " \n\t".contains(char) && !token.eq("") {
-            tokens.push(Token {
-                value: token.clone(),
-            });
-            token.clear();
-        }
-        if "()[]{}+-*/,;.:=!?&|\"'#^%~".contains(char) {
-            if !token.is_empty() {
-                tokens.push(Token {
-                    value: token.clone(),
-                });
-                token.clear();
+        if char == '\n' || char == '\t' || char.is_whitespace() {
+            push_token(&mut token, &mut tokens, offset, line_counter);
+            push_token(&mut char_seq, &mut tokens, offset, line_counter);
+            if char == '\n' {
+                offset += 1;
+                line_counter = offset;
+                continue;
             }
-            if !char_seq.is_empty() {
-                if char_seq.contains(char) && !"{}".contains(char){
-                    char_seq.push(char);
-                }
-                else {
-                    tokens.push(Token {
-                        value: char_seq.clone(),
-                    });
-                    char_seq.clear();
-                    char_seq.push(char);
-                }
+        }
+        else if "()[]{}+-*/,;.:=!?&|\"'#^%~".contains(char) {
+            push_token(&mut token, &mut tokens, offset, line_counter);
+            if !char_seq.is_empty() && char_seq.contains(char) && !"{}".contains(char){
+                char_seq.push(char);
             }
             else {
+                push_token(&mut char_seq, &mut tokens, offset, line_counter);
                 char_seq.push(char);
             }
         }
         else if char.is_alphanumeric() {
-            if !char_seq.is_empty() {
-                tokens.push(Token {
-                    value: char_seq.clone(),
-                });
-                char_seq.clear();
-            }
+            push_token(&mut char_seq, &mut tokens, offset, line_counter);
             token.push(char);
         }
+
+        // Used to keep track of the char's position in the file
+        offset += 1;
     }
 
+    push_token(&mut token, &mut tokens, offset, line_counter);
+    push_token(&mut char_seq, &mut tokens, offset, line_counter);
+
     Ok(tokens)
+}
+
+fn push_token(token: &mut String, tokens: &mut Vec<Token>, offset: u64, line: u64) {
+    if !token.is_empty() {
+        tokens.push(Token::new(token.clone(), offset, line));
+        token.clear();
+    }
 }
--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -1,4 +1,20 @@
 #[derive(Debug)]
 pub struct Token {
     pub value: String,
+    pub position: u64,
+    pub line: u64,
+}
+
+impl Token {
+    pub fn new(
+        value: String,
+        position: u64,
+        line: u64
+    ) -> Token {
+        Token {
+            value,
+            position,
+            line
+        }
+    }
 }
